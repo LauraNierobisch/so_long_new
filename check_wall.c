b63983532f0e49cb056d40be04c6a96471fbd2e4
Laura Nierobisch <lnierobi@2-E-4.42heilbronn.de>
2024-10-11 09:40:34 +0200

    part of map validation

---
+++ check_wall.c
@@ -0,0 +1,58 @@
+#include <so_long.h>
+
+bool	top_wall(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (game->map[0][i])
+	{
+		if (game->map[0][i] != '1')
+			return (false)
+	}
+	return (true);
+}
+
+bool	down_wall(t_game *game)
+{
+	int	i;
+	int	last;
+
+	i = 0;
+	last = 0;
+	while (game->map[last + 1] != NULL)
+		last++;
+	while (game->map[last][i] != '\0')
+	{
+		if (game->map[last][i] != '1')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+bool	right_side_wall(t_stuct *game)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(game->map[i]);
+	while (game->map[i][len - 1])
+	{
+		if (game->map[i][len - 1] != '1')
+		{
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+bool	left_side_wall(t_struct *game)
+{
+	int i;
+	i = 0;
+	if (game->map[i][0] != '1')
+		return (false);
+	i++;
+	return (true);
+}
