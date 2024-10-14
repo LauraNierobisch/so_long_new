2e29860cd1d63e10e1d66db6c40878d61bb0fdad
Laura Nierobisch <lnierobi@2-E-4.42heilbronn.de>
2024-10-14 08:58:00 +0200

    map rendering

---
+++ map_validation.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_validation.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnierobi <lnierobi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 08:55:19 by lnierobi          #+#    #+#             */
+/*   Updated: 2024/10/14 08:55:52 by lnierobi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <so_long.h>
+
+bool	exit_valid(t_game *game)
+{
+	int	e_found;
+	int	i;
+	int	j;
+
+	e_found = 0;
+	i = 0;
+	j = 1;
+	while (game->map[i])
+	{
+		while (game->map[i][j])
+		{
+			if (game->map[i][j] == 'E')
+				e_found++;
+			j++;
+		}
+		i++;
+	}
+	if (e_found != 1)
+	{
+		return (false);
+	}
+	return (true);
+}
+
+bool	player_valid(t_game *game)
+{
+	int	p_found;
+	int	i;
+	int	j;
+
+	p_found = 0;
+	i = 0;
+	j = 1;
+	while (game->map[i])
+	{
+		while (game->map[i][j] == 'P')
+			p_found++;
+		j++;
+	}
+	i++;
+	if (p_found != 1)
+	{
+		return (false);
+	}
+	return (true);
+}
+bool	colect_valid(t_game *game)
+{
+	int c_found = 0;
+	int i = 0;
+	int j = 1;
+
+	while (game->map[i])
+	{
+		while (game->map[i][j] == 'C')
+			c_found++;
+		j++;
+	}
+	i++;
+	if (c_found < 1)
+	{
+		return (false);
+	}
+	return (true);
+}
